25b1e8bb9221e7d4dd183202f101a8fcb3868393
YanRuby <koobrsamb9512295122@gmail.com>
2023-03-04 14:10:13 +0800

    Modify : 函式 方法

--- practise1.c
+++ practise1.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int  base, exponent ,result;
+    int  i , base , exponent ,result;
     printf("================================\n");
     printf("|> ED1 Power Function\n");
     printf("================================\n");
     printf("Please Enter base and power.\n");
     printf("* * * * * * * * * * * * * * * *\n");
-
+    result=1;
     scanf("%d %d", &base, &exponent);
+    for(i=1;i<=exponent; i=i+1){
+        result=result*base;
+    }
+    
     if ( base != 0 ){
-        result = pow(base,exponent);
+        
         printf(">> Pow( %d, %d ) = %d\n", base, exponent, result);
     }
     else if( base == 0 & exponent == 0){
