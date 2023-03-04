b83afc68f974bfc7f0ca7d436791293533c17871
YanRuby <koobrsamb9512295122@gmail.com>
2023-03-04 14:43:41 +0800

    Optimize : 等級換算 基礎: v1.2.0

--- practise3.c
+++ practise3.c
@@ -5,11 +5,17 @@ int main(){
     printf("================================\n");
     printf("Please Enter score.\n");
     scanf("%d", &score);
+    if (score < 0 | score > 100){
+        score = 111;
+    }
     level = score / 10;
     printf("================================\n");
 
     switch(level)
     {
+        case 11:
+            printf("Does not exist!!!!!\n");
+            break;
         case 10:
             printf("A\n");
             break;
